1f935a995355ea886500a444bd80b7d26025bda8
leoSilvaWarren <leonardo.silva@warren.com.br>
2022-06-29 16:23:16 -0300

    feat: tests widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,60 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:golden_tests/main.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> theAppIsRendered(WidgetTester tester) async {
+  await loadAppFonts();
+  await tester.pumpWidgetBuilder(const MyApp());
+}
+
+Future<void> deviceBuilder(WidgetTester tester) async {
+  await loadAppFonts();
+  final builder = DeviceBuilder()
+    ..overrideDevicesForAllScenarios(devices: [
+      Device.iphone11,
+      const Device(size: Size(420, 750), name: 'Android 6'),
+      Device.phone,
+      const Device(size: Size(400, 600), name: 'Android 5')
+  ])
+    ..addScenario(widget: const MyApp());
+  await tester.pumpDeviceBuilder(builder);
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('WHEN load initial page THEN counter should not 1', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
+  });
 
-    // Tap the '+' icon and trigger a frame.
+  testWidgets('WHEN load initial page THEN counter must be 0', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('0'), findsOneWidget);
+  });
+
+  testWidgets('WHEN tap the "+" page THEN counter should not 0', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
+  });
+
+  testWidgets('WHEN tap the "+" page THEN counter must be 1', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
     expect(find.text('1'), findsOneWidget);
   });
+
+  testGoldens("WHEN render app THEN screenshot verified device", (WidgetTester tester) async {
+    await deviceBuilder(tester);
+    await screenMatchesGolden(tester, 'device_counter');
+  });
+
+  testGoldens("WHEN render app THEN screenshot verified snapshot", (WidgetTester tester) async {
+     await theAppIsRendered(tester);
+     await screenMatchesGolden(tester, 'snapshot_counter');
+  });
+
 }
